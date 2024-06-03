edba674fc4a794ea41e5f06067028021c32ed55e
KobzinK <roflancanek@gmail.com>
2024-06-03 11:28:36 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+﻿const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path'); // Додайте цей рядок
+
+const app = express();
+
+app.use(express.json());
+
+// Розміщення статичного контенту
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ваші існуючі маршрути CRUD і обробники помилок тут...
+
+const startServer = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected...');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+startServer();
